c3ffd1fb4262bad215834d689d105237a59f4886
Jichao Ouyang <oyanglulu@gmail.com>
2014-02-26 12:47:16 +0800

    add spinner

--- src/main.js
+++ src/main.js
@@ -6,13 +6,14 @@ $(function(){
 		kanban = null;		
 	}
 	if (!github.checkLogin()) {
-		$(".site.clearfix").html(nunjucks.render('src/templates/index.html'));
+		$(".site.clearfix").html(mynunjucks.render('src/templates/index.html'));
 		$('#try-gira').click(function () {
 			var userrepo = $(".marketing-section-enterprise input[name=username]").val().split('/');
 			github.owner = userrepo[0];
 			github.owner = userrepo[1];
 			renderViews();
 		});
+		renderViews();
 	}
 	github.getAccessToken().then(function () {
 		renderViews();
