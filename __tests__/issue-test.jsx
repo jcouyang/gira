111e88a18baef856df9917f68c27fd2258e871ff
Jichao Ouyang <oyanglulu@gmail.com>
2014-08-04 20:23:23 +0800

    add test for issue.jxs

---
+++ __tests__/issue-test.jsx
@@ -0,0 +1,26 @@
+jest.dontMock('../src/issue')
+describe('Issue', function(){
+	var React = require('react/addons');
+	var $ = require('jquery');
+	var Issue = require('../src/issue');
+	var TestUtils = React.addons.TestUtils;
+	var issue = TestUtils.renderIntoDocument(
+		<Issue labels={["hehe"]} name="hehe" number="123" url="http://oyanglul.us" title="fakeIssue" repo="gira" owner="jcouyang" label="hehe"/>
+	)
+	it('set data when drag', function(){
+		var issueElement = TestUtils.findRenderedComponentWithType(issue, Issue);
+		TestUtils.Simulate.drag(issueElement);
+		expect(issueElement.getDOMNode().id).toBe("issue-123")
+	})
+
+	it('reveal issue to popup', function(){
+		var linkElement = TestUtils.findRenderedDOMComponentWithClass(issue, "popable");
+		var jqueryObj = jest.genMockFn();
+		jqueryObj.attr = jest.genMockFn();
+		jqueryObj.load = jest.genMockFn();
+		jqueryObj.attr.mockReturnValue("#hehe")
+		$.mockReturnValue(jqueryObj)
+		TestUtils.Simulate.click(linkElement, {currentTarget:"#issue-123"});
+		expect(jqueryObj.load).toBeCalledWith("hehe #issues_next")
+	})
+})
