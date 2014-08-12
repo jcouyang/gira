c73e60c3d41b6278b88b0190959fa14f906fab12
Jichao Ouyang <oyanglulu@gmail.com>
2014-08-12 21:34:06 +1000

    grease monkey header

---
+++ javascripts/gira.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name gira
+// @namespace oyanglul.us
+// @description gira test
+// @include https://github.com/*/*/issues*
+// @include http://getgira.github.io/?code*
+// @version 1
+// @grant GM_xmlhttpRequest
+// @grant GM_addStyle
+// @grant GM_getResourceText
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_deleteValue
+// @resource css https://rawgit.com/jcouyang/gira/master/stylesheets/gira.css
+// ==/UserScript==
+var css = GM_getResourceText('css');
+GM_addStyle(css);
+console.log('resource loaded');
