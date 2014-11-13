aab59018cb90ebcd184b4aff5d326f5469c5168a
Jichao Ouyang <oyanglulu@gmail.com>
2014-11-13 16:48:58 +0800

    add  page icon

---
+++ chrome/background.js
@@ -0,0 +1,19 @@
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'github.com',  pathSuffix: 'issues' },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
+
